e0c6c0608bcb0f9e7b544b6b0d2f3a25573d22b0
donglin0 <44114683+donglin0@users.noreply.github.com>
2019-12-24 08:37:27 +0800

    03

--- InsertVertex.cpp
+++ InsertVertex.cpp
@@ -3,7 +3,7 @@
 //插入顶点
 void InsertVertex(AdjMGraph* G, DataType vertex)
 {
-	//cout << "李太龙" << endl;
+	
 	if (ListInsert(&G->vertices, G->vertices.size, vertex) == 0)//在顶点顺序表的表尾插入顶点vertex
 	{
 		printf("插入顶点时空间已满无法插入！");
